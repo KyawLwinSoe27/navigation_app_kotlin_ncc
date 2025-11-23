123b6a050a8f9b32e59ae89aa938c7b10246a780
KyawLwinSoe27 <soek38902@gmail.com>
2025-11-23 11:18:20 +0630

    updated

--- app/src/main/java/com/example/navigationapp/UserActivity.kt
+++ app/src/main/java/com/example/navigationapp/UserActivity.kt
@@ -1,7 +1,10 @@
 package com.example.navigationapp
 
+import android.content.Intent
 import android.os.Bundle
 import android.util.Log
+import android.view.Menu
+import android.view.MenuItem
 import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
@@ -10,6 +13,7 @@ import androidx.core.view.WindowInsetsCompat
 import androidx.navigation.NavController
 import androidx.navigation.fragment.NavHostFragment
 import androidx.navigation.ui.setupActionBarWithNavController
+import androidx.navigation.ui.setupWithNavController
 import com.example.navigationapp.databinding.ActivityUserBinding
 
 class UserActivity : AppCompatActivity() {
@@ -34,11 +38,49 @@ class UserActivity : AppCompatActivity() {
         val navHostFragment = supportFragmentManager.findFragmentById(R.id.user_fragment_container) as NavHostFragment
         navController = navHostFragment.navController
 
-        setupActionBarWithNavController(navController)
+//        setupActionBarWithNavController(navController)
 
+        binding.bottomNavView.setupWithNavController(navController)
+        navController.navigate(R.id.welcomeUserFragment, bundle)
+        binding.bottomNavView.setOnItemSelectedListener {
+            item ->
+            when(item.itemId) {
+                R.id.itemWelcome -> {
+                    navController.navigate(R.id.welcomeUserFragment, bundle)
+                    true
+                }
+                R.id.itemEntry -> {
+                    navController.navigate(R.id.entryFragment, bundle)
+                    true
+                }
+                R.id.itemListView -> {
+                    navController.navigate(R.id.listViewFragment, bundle)
+                    true
+                }
+                else -> false
+            }
+        }
     }
 
     override fun onSupportNavigateUp(): Boolean {
         return navController.navigateUp() || super.onSupportNavigateUp()
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.user_menu, menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        val result = when(item.itemId) {
+            R.id.itemLogout -> {
+                intent = Intent(this, MainActivity::class.java)
+                startActivity(intent)
+                finish()
+                true
+            }
+            else -> false
+        }
+        return result
     }
 }
